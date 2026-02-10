f20b26ac48a92f1255d0e7db3627cf2f3d8fcd2c
joncarl-15 <joncarlomarasigan15@gmail.com>
2026-02-10 09:50:51 +0800

    Added Windows Support

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose';
+import dns from 'dns';
 import logger from '../utils/logger';
 import { env } from './env';
 
 export const connectDB = async (): Promise<void> => {
+    if (process.platform === 'win32') {
+        dns.setServers(['8.8.8.8', '8.8.4.4']);
+    }
     try {
         const conn = await mongoose.connect(env.MONGO_URI);
         logger.info(`MongoDB Connected: ${conn.connection.host}`);
